349fb16cc3e3a877d24976e9c5eba547b20bdfcf
magu12 <maggubadov@gmail.com>
2018-11-21 15:00:44 +0300

    StudentForm Search Add

--- HostelGGMC/StudentForm.Designer.cs
+++ HostelGGMC/StudentForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.dgStudent = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -38,8 +39,33 @@
             this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.cbEvited = new System.Windows.Forms.CheckBox();
+            this.cbNotInhabited = new System.Windows.Forms.CheckBox();
+            this.cbInhabited = new System.Windows.Forms.CheckBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.tbGroupNomber = new System.Windows.Forms.TextBox();
+            this.cbGroupName = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tbStudent = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbStage = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbRoom = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbDefault = new System.Windows.Forms.LinkLabel();
+            this.buttonDeleteStudent = new System.Windows.Forms.Button();
+            this.buttonAddStudent = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgStudent)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgStudent
@@ -59,16 +85,15 @@
             this.Column8,
             this.Column6,
             this.Column7});
-            this.dgStudent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgStudent.Dock = System.Windows.Forms.DockStyle.Left;
             this.dgStudent.Location = new System.Drawing.Point(0, 0);
             this.dgStudent.MultiSelect = false;
             this.dgStudent.Name = "dgStudent";
             this.dgStudent.ReadOnly = true;
             this.dgStudent.RowHeadersVisible = false;
             this.dgStudent.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgStudent.Size = new System.Drawing.Size(800, 450);
-            this.dgStudent.TabIndex = 2;
-            this.dgStudent.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dgStudent_KeyDown);
+            this.dgStudent.Size = new System.Drawing.Size(827, 409);
+            this.dgStudent.TabIndex = 4;
             // 
             // Column1
             // 
@@ -109,7 +134,7 @@
             // 
             // Column6
             // 
-            this.Column6.HeaderText = "Курс";
+            this.Column6.HeaderText = "Номер группы";
             this.Column6.Name = "Column6";
             this.Column6.ReadOnly = true;
             // 
@@ -119,11 +144,245 @@
             this.Column7.Name = "Column7";
             this.Column7.ReadOnly = true;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox5);
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.lbDefault);
+            this.groupBox1.Location = new System.Drawing.Point(833, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 339);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Параметры поиска:";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.cbEvited);
+            this.groupBox5.Controls.Add(this.cbNotInhabited);
+            this.groupBox5.Controls.Add(this.cbInhabited);
+            this.groupBox5.Location = new System.Drawing.Point(6, 239);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(228, 71);
+            this.groupBox5.TabIndex = 12;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Заселён/не заселён, выселен:";
+            // 
+            // cbEvited
+            // 
+            this.cbEvited.AutoSize = true;
+            this.cbEvited.Location = new System.Drawing.Point(74, 42);
+            this.cbEvited.Name = "cbEvited";
+            this.cbEvited.Size = new System.Drawing.Size(71, 17);
+            this.cbEvited.TabIndex = 2;
+            this.cbEvited.Text = "Выселен";
+            this.cbEvited.UseVisualStyleBackColor = true;
+            this.cbEvited.CheckedChanged += new System.EventHandler(this.cbEvited_CheckedChanged);
+            // 
+            // cbNotInhabited
+            // 
+            this.cbNotInhabited.AutoSize = true;
+            this.cbNotInhabited.Location = new System.Drawing.Point(142, 19);
+            this.cbNotInhabited.Name = "cbNotInhabited";
+            this.cbNotInhabited.Size = new System.Drawing.Size(85, 17);
+            this.cbNotInhabited.TabIndex = 1;
+            this.cbNotInhabited.Text = "Не заселён";
+            this.cbNotInhabited.UseVisualStyleBackColor = true;
+            this.cbNotInhabited.CheckedChanged += new System.EventHandler(this.cbNotInhabited_CheckedChanged);
+            // 
+            // cbInhabited
+            // 
+            this.cbInhabited.AutoSize = true;
+            this.cbInhabited.Location = new System.Drawing.Point(6, 19);
+            this.cbInhabited.Name = "cbInhabited";
+            this.cbInhabited.Size = new System.Drawing.Size(69, 17);
+            this.cbInhabited.TabIndex = 0;
+            this.cbInhabited.Text = "Заселён";
+            this.cbInhabited.UseVisualStyleBackColor = true;
+            this.cbInhabited.CheckedChanged += new System.EventHandler(this.cbInhabited_CheckedChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tbGroupNomber);
+            this.groupBox4.Controls.Add(this.cbGroupName);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Location = new System.Drawing.Point(6, 165);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(228, 65);
+            this.groupBox4.TabIndex = 11;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "По группе и(или) курсу:";
+            // 
+            // tbGroupNomber
+            // 
+            this.tbGroupNomber.Location = new System.Drawing.Point(122, 33);
+            this.tbGroupNomber.Name = "tbGroupNomber";
+            this.tbGroupNomber.Size = new System.Drawing.Size(100, 20);
+            this.tbGroupNomber.TabIndex = 11;
+            this.tbGroupNomber.TextChanged += new System.EventHandler(this.tbGroupNomber_TextChanged);
+            // 
+            // cbGroupName
+            // 
+            this.cbGroupName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbGroupName.FormattingEnabled = true;
+            this.cbGroupName.Items.AddRange(new object[] {
+            "ПК",
+            "ПСК",
+            "ЭК",
+            "ЛЧС",
+            "ТОС",
+            "ТОМ"});
+            this.cbGroupName.Location = new System.Drawing.Point(6, 32);
+            this.cbGroupName.Name = "cbGroupName";
+            this.cbGroupName.Size = new System.Drawing.Size(108, 21);
+            this.cbGroupName.TabIndex = 8;
+            this.cbGroupName.SelectedIndexChanged += new System.EventHandler(this.cbGroupName_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Группа";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(130, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Номер группы";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tbStudent);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(6, 91);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(228, 68);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "По личным атрибутам студента:";
+            // 
+            // tbStudent
+            // 
+            this.tbStudent.Location = new System.Drawing.Point(6, 37);
+            this.tbStudent.Name = "tbStudent";
+            this.tbStudent.Size = new System.Drawing.Size(216, 20);
+            this.tbStudent.TabIndex = 10;
+            this.tbStudent.TextChanged += new System.EventHandler(this.tbStudent_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ФИО:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbStage);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.cbRoom);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(228, 66);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "По этажу и блоку:";
+            // 
+            // cbStage
+            // 
+            this.cbStage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbStage.FormattingEnabled = true;
+            this.cbStage.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cbStage.Location = new System.Drawing.Point(6, 33);
+            this.cbStage.Name = "cbStage";
+            this.cbStage.Size = new System.Drawing.Size(108, 21);
+            this.cbStage.TabIndex = 4;
+            this.cbStage.SelectedIndexChanged += new System.EventHandler(this.cbStage_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Комната";
+            // 
+            // cbRoom
+            // 
+            this.cbRoom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbRoom.FormattingEnabled = true;
+            this.cbRoom.Location = new System.Drawing.Point(120, 33);
+            this.cbRoom.Name = "cbRoom";
+            this.cbRoom.Size = new System.Drawing.Size(102, 21);
+            this.cbRoom.TabIndex = 5;
+            this.cbRoom.SelectedIndexChanged += new System.EventHandler(this.cbRoom_SelectedIndexChanged);
+            this.cbRoom.Enter += new System.EventHandler(this.cbRoom_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Этаж";
+            // 
+            // lbDefault
+            // 
+            this.lbDefault.AutoSize = true;
+            this.lbDefault.Location = new System.Drawing.Point(96, 313);
+            this.lbDefault.Name = "lbDefault";
+            this.lbDefault.Size = new System.Drawing.Size(55, 13);
+            this.lbDefault.TabIndex = 8;
+            this.lbDefault.TabStop = true;
+            this.lbDefault.Text = "Сбросить";
+            // 
+            // buttonDeleteStudent
+            // 
+            this.buttonDeleteStudent.Location = new System.Drawing.Point(833, 374);
+            this.buttonDeleteStudent.Name = "buttonDeleteStudent";
+            this.buttonDeleteStudent.Size = new System.Drawing.Size(240, 23);
+            this.buttonDeleteStudent.TabIndex = 15;
+            this.buttonDeleteStudent.Text = "Удалить";
+            this.buttonDeleteStudent.UseVisualStyleBackColor = true;
+            this.buttonDeleteStudent.Click += new System.EventHandler(this.buttonDeleteStudent_Click);
+            // 
+            // buttonAddStudent
+            // 
+            this.buttonAddStudent.Location = new System.Drawing.Point(833, 345);
+            this.buttonAddStudent.Name = "buttonAddStudent";
+            this.buttonAddStudent.Size = new System.Drawing.Size(240, 23);
+            this.buttonAddStudent.TabIndex = 14;
+            this.buttonAddStudent.Text = "Добавить";
+            this.buttonAddStudent.UseVisualStyleBackColor = true;
+            this.buttonAddStudent.Click += new System.EventHandler(this.buttonAddStudent_Click);
+            // 
             // StudentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1078, 409);
+            this.Controls.Add(this.buttonDeleteStudent);
+            this.Controls.Add(this.buttonAddStudent);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dgStudent);
             this.Name = "StudentForm";
             this.ShowIcon = false;
@@ -132,12 +391,22 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StudentForm_FormClosing);
             this.Load += new System.EventHandler(this.StudentForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgStudent)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
+        private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.DataGridView dgStudent;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
@@ -147,6 +416,26 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
-        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.CheckBox cbEvited;
+        private System.Windows.Forms.CheckBox cbNotInhabited;
+        private System.Windows.Forms.CheckBox cbInhabited;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox tbGroupNomber;
+        private System.Windows.Forms.ComboBox cbGroupName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tbStudent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cbStage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbRoom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel lbDefault;
+        private System.Windows.Forms.Button buttonDeleteStudent;
+        private System.Windows.Forms.Button buttonAddStudent;
     }
 }
--- HostelGGMC/StudentForm.cs
+++ HostelGGMC/StudentForm.cs
@@ -15,6 +15,92 @@ namespace HostelGGMC
     public partial class StudentForm : Form
     {
         StudentController studentController = new StudentController();
+        RoomController roomController = new RoomController();
+
+        public void DoOperationsFilter()
+        {
+            var student = studentController.GetAllStudents();
+
+            dgStudent.Rows.Clear();
+
+            if (cbStage.SelectedIndex >= 0)
+            {
+                var filter =
+                from d in student
+                where d.Room.ToString()[0] == cbStage.SelectedItem.ToString()[0]
+                select d;
+                student = filter;
+            }
+            if (cbRoom.SelectedIndex >= 0 && cbStage.SelectedIndex >= 0)
+            {
+                var filter =
+                from d in student
+                where d.Room.ToString() == (cbStage.SelectedItem.ToString() + "-" + cbRoom.SelectedItem.ToString())
+                select d;
+                student = filter;
+            }
+            if (tbStudent.Text.Length >= 1)
+            {
+                var filter =
+                from d in student
+                where d.StudentName.StartsWith(tbStudent.Text)
+                select d;
+                student = filter;
+            }
+            if (cbGroupName.SelectedIndex >= 0)
+            {
+                var filter =
+                from d in student
+                where d.GroupName.ToString() == cbGroupName.SelectedItem.ToString()
+                select d;
+                student = filter;
+            }
+            if (tbGroupNomber.Text.Length >= 1)
+            {
+                var filter =
+                from d in student
+                where d.GroupNumber.ToString().StartsWith(tbGroupNomber.Text)
+                select d;
+                student = filter;
+            }
+            if (cbInhabited.Checked == true)
+            {
+                var filter =
+                from d in student
+                where d.Inhabited == 1
+                select d;
+                student = filter;
+            }
+            if (cbNotInhabited.Checked == true)
+            {
+                var filter =
+                from d in student
+                where d.Inhabited == 0
+                select d;
+                student = filter;
+            }
+            if (cbEvited.Checked == true)
+            {
+                var filter =
+                from d in student
+                where d.Envicted == 1
+                select d;
+                student = filter;
+            }
+
+
+
+            foreach (Student students in student)
+            {
+                if (students.RoomType == 1)
+                    dgStudent.Rows.Add(students.Id, students.StudentName, students.Room,
+                       students.Inhabited, students.Envicted, students.GroupName, students.GroupNumber, "Двойка");
+                if (students.RoomType == 0)
+                    dgStudent.Rows.Add(students.Id, students.StudentName, students.Room,
+                      students.Inhabited, students.Envicted, students.GroupName, students.GroupNumber, "Тройка");
+            }
+        }
+
         public StudentForm()
         {
             InitializeComponent();
@@ -33,6 +119,18 @@ namespace HostelGGMC
                     dgStudent.Rows.Add(student.Id, student.StudentName, student.Room,
                       student.Inhabited, student.Envicted, student.GroupName, student.GroupNumber, "Тройка");
             }
+        }
+
+        private void DefaultForm()
+        {
+            cbRoom.SelectedItem = null;
+            cbStage.SelectedItem = null;
+            cbGroupName.SelectedItem = null;
+            tbGroupNomber.Text = null;
+            tbStudent.Text = null;
+            cbInhabited.Checked = false;
+            cbNotInhabited.Checked = false;
+            cbEvited.Checked = false;
         }
 
         private void dgStudent_KeyDown(object sender, KeyEventArgs e)
@@ -61,6 +159,99 @@ namespace HostelGGMC
             this.Hide();
             ShowBaseMenu showBaseMenu = new ShowBaseMenu();
             showBaseMenu.Show();
+        }
+
+        private void cbStage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                if (cbStage.SelectedItem != null)
+                {
+                    DoOperationsFilter();
+                }
+        }
+
+        private void cbRoom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbRoom.SelectedItem != null)
+            {
+                DoOperationsFilter();
+            }
+        }
+
+        private void cbRoom_Enter(object sender, EventArgs e)
+        {
+            var rooms = roomController.RoomSearch(int.Parse(cbStage.SelectedItem.ToString()));
+            cbRoom.Items.Clear();
+            foreach (Room rm in rooms)
+                cbRoom.Items.Add(rm.Number);
+        }
+
+        private void tbStudent_TextChanged(object sender, EventArgs e)
+        {
+            DoOperationsFilter();
+        }
+
+        private void cbGroupName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbGroupName.SelectedItem != null)
+            {
+                DoOperationsFilter();
+            }
+        }
+
+        private void tbGroupNomber_TextChanged(object sender, EventArgs e)
+        {
+            if (tbGroupNomber.Text != null)
+            {
+                DoOperationsFilter();
+            }
+        }
+
+        private void cbNotInhabited_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbNotInhabited.Checked == true)
+            {
+                cbInhabited.Checked = false; cbEvited.Checked = false;
+                DoOperationsFilter();
+            }
+            else if (cbInhabited.Checked == false && cbEvited.Checked == false) { DefaultForm(); StudentForm_Load(sender, e); }
+        }
+
+        private void cbInhabited_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbInhabited.Checked == true)
+            {
+                cbNotInhabited.Checked = false; cbEvited.Checked = false;
+                DoOperationsFilter();
+            }
+            else if (cbNotInhabited.Checked == false && cbEvited.Checked == false) { DefaultForm(); StudentForm_Load(sender, e); };
+        }
+
+        private void cbEvited_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbEvited.Checked == true)
+            {
+                cbInhabited.Checked = false; cbNotInhabited.Checked = false;
+                DoOperationsFilter();
+            }
+            else if (cbNotInhabited.Checked == false && cbInhabited.Checked == false) { DefaultForm(); StudentForm_Load(sender, e); }
+        }
+
+        private void buttonAddStudent_Click(object sender, EventArgs e)
+        {
+            StudentAddForm studentAddForm = new StudentAddForm();
+            studentAddForm.ShowDialog();
+            StudentForm_Load(sender, e);
+        }
+
+        private void buttonDeleteStudent_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Потвердите удаление.", "Подтверждение", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                studentController.DeleteStudent(int.Parse(dgStudent[0, dgStudent.SelectedRows[0].Index].Value.ToString()));
+                StudentForm_Load(sender, e);
+
+            }
         }
     }
 }
